33d6e3b8eccb606e7d39b564d9a461829a36d826
KumJungMin <rmawjdals@naver.com>
2024-03-03 21:31:35 +0900

    feat: 현 workspace의 컬러변수 불러오기

--- src/SidebarProvider.ts
+++ src/SidebarProvider.ts
@@ -3,14 +3,19 @@ import * as vscode from "vscode";
 export class SidebarProvider implements vscode.WebviewViewProvider {
   _view?: vscode.WebviewView;
   _doc?: vscode.TextDocument;
+  _docText?: any;
+  _baseColorFile?: string;
 
-  constructor(private readonly _extensionUri: vscode.Uri) {}
+  constructor(private readonly _extensionUri: vscode.Uri ) {
+    (async () => {
+      this._docText = await this.getColorVariables();
+    })();
+  }
 
   public resolveWebviewView(webviewView: vscode.WebviewView) {
     this._view = webviewView;
 
     webviewView.webview.options = {
-      // Allow scripts in the webview
       enableScripts: true,
       localResourceRoots: [this._extensionUri],
     };
@@ -41,6 +46,47 @@ export class SidebarProvider implements vscode.WebviewViewProvider {
     this._view = panel;
   }
 
+  public async getColorVariables() {
+    const editor = vscode.window.activeTextEditor;
+    if (editor) {
+      const rootPath = vscode.workspace.workspaceFolders?.[0];
+
+      // TODO: 파일 찾는 로직 수정하기 - 외부에서 파일 경로를 지정할 수 있게
+      const baseColorFiles = [
+        `${rootPath?.uri.path}/color.scss`,
+        `${rootPath?.uri.path}/src/colors.scss`,
+        `${rootPath?.uri.path}/src/styles/color.scss`,
+      ];
+      const colorFile = baseColorFiles.find((file) => {
+        return vscode.workspace.textDocuments.find((doc) =>  doc.uri.path === file);
+      });
+      if (colorFile) {
+        this._baseColorFile = colorFile;
+        const document = await vscode.workspace.openTextDocument(colorFile);
+        return this.formatColorVariables(document.getText());
+      }
+    }
+  }
+
+  private formatColorVariables(text: string) {
+    const variableRegex = /(--color-.*:.*;)/g;  // TODO: 변수명 수정하기
+    const colorVariables = text.matchAll(variableRegex);
+
+    // TODO: 변수가 하나로 합쳐지는 문제 해결하기
+    const colorVariableArr = colorVariables.toString();
+
+    let colorText = [];
+    for (const color of colorVariables) {
+      const [key, value] = color.toString().split(":");
+      colorText.push(`<p class="color-variable">
+        <span class="color-variable__name">${key}</span>
+        <span class="color-variable__value">${value}</span>
+        <div style="width: 10px; height: 10px; background-color: ${value}"></div>
+      </p>`);
+    }
+    return colorText.join("");
+  }
+
   private _getHtmlForWebview(webview: vscode.Webview) {
     const styleResetUri = webview.asWebviewUri(
       vscode.Uri.joinPath(this._extensionUri, "media", "reset.css")
@@ -48,11 +94,7 @@ export class SidebarProvider implements vscode.WebviewViewProvider {
     const styleVSCodeUri = webview.asWebviewUri(
       vscode.Uri.joinPath(this._extensionUri, "media", "vscode.css")
     );
-    const bodyScriptUri = webview.asWebviewUri(
-      vscode.Uri.joinPath(this._extensionUri, "media", "sidebar.js")
-    );
 
-    // TODO: 사이드바 로드 에러 확인하기
     return `<!DOCTYPE html>
     <html lang="en">
       <head>
@@ -63,7 +105,9 @@ export class SidebarProvider implements vscode.WebviewViewProvider {
         <title>Welcome to GitOps - New User Guide</title>
       </head>
       <body>
-        <script src="${bodyScriptUri}"></script>
+      // TODO: 검색 기능 추가하기
+      // TODO: refresh 버튼 추가하기
+      ${this._docText}
       </body>
     </html>`;
       
--- src/extension.ts
+++ src/extension.ts
@@ -7,8 +7,19 @@ export function activate(context: vscode.ExtensionContext) {
 	const testCommand = vscode.commands.registerCommand('colorvariabletracker.helloWorld', () => {
 		vscode.window.showInformationMessage('Hello World from colorVariableTracker!');
 	});
+
+
 	const sidebarProvider = new SidebarProvider(context.extensionUri);
-	
+
+	// TODO: 이벤트 미감지 확인하기
+	vscode.workspace.onDidChangeTextDocument((e) => {
+		if (e.document.uri.path === sidebarProvider._baseColorFile) {
+			sidebarProvider._docText = e.document.getText();
+			sidebarProvider._view?.webview.postMessage({ type: "update", value: sidebarProvider._docText });
+		}
+	}
+	);
+
 	context.subscriptions.push(testCommand);
 	context.subscriptions.push(
     vscode.window.registerWebviewViewProvider("colorVariableTrackerSidebar", sidebarProvider)
