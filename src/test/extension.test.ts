94c388fdaddadcf9a732530beee097e0b6d4d71b
KumJungMin <rmawjdals@naver.com>
2024-03-23 19:01:54 +0900

    test: color path 테스트 추가하기

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -1,15 +1,33 @@
 import * as assert from 'assert';
-
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
 import * as vscode from 'vscode';
-// import * as myExtension from '../../extension';
+import { SidebarProvider } from '../SidebarProvider';
+import * as sinon from 'sinon';
 
-suite('Extension Test Suite', () => {
-	vscode.window.showInformationMessage('Start all tests.');
+suite('SidebarProvider Tests', () => {
 
-	test('Sample test', () => {
-		assert.strictEqual(-1, [1, 2, 3].indexOf(5));
-		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
-	});
+  test('the colorFilePath set as the path in config.', async () => {
+    // Arrange
+    const extensionUri = vscode.Uri.file(__dirname);
+    const sidebarProvider = new SidebarProvider(extensionUri);
+    sidebarProvider['_baseFilePath'] = extensionUri.path;
+    sinon.stub(vscode.workspace, 'getConfiguration').returns({
+      get: () => '/test.css',
+      has: function (section: string): boolean {
+        throw new Error('Function not implemented.');
+      },
+      inspect: function <T>(section: string): { key: string; defaultValue?: T | undefined; globalValue?: T | undefined; workspaceValue?: T | undefined; workspaceFolderValue?: T | undefined; defaultLanguageValue?: T | undefined; globalLanguageValue?: T | undefined; workspaceLanguageValue?: T | undefined; workspaceFolderLanguageValue?: T | undefined; languageIds?: string[] | undefined; } | undefined {
+        throw new Error('Function not implemented.');
+      },
+      update: function (section: string, value: any, configurationTarget?: boolean | vscode.ConfigurationTarget | null | undefined, overrideInLanguage?: boolean | undefined): Thenable<void> {
+        throw new Error('Function not implemented.');
+      }
+    });
+
+    // Act
+    await sidebarProvider.registerColorFilePath();
+
+    // Assert
+    assert.strictEqual(sidebarProvider._colorFilePath, `${extensionUri.path}/test.css`);
+
+  });
 });
